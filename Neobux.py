0880692cd352362f6ecd333c7f3882cb3e0fee08
Djaenk <10876318+Djaenk@users.noreply.github.com>
2019-06-04 16:37:39 -0500

    Initial attempt to add login logic to gui
    Still not functional, work in progress

--- Neobux.py
+++ Neobux.py
@@ -15,9 +15,9 @@ from io import BytesIO
 
 class Neobux:
 
-    _AD_HEADER = (By.XPATH, '/html/body/table/tbody/tr[1]/td/table/tbody/tr/td/table/tbody/tr/td[1]/table/tbody/tr')
-    _LOGIN_ROWS_XPATH = './table/tbody/tr[1]/td/table/tbody/tr'
-    _DASHBOARD = (By.XPATH, '/html/body/div[2]/div/table')
+    _AD_HEADER = (By.XPATH, "/html/body/table/tbody/tr[1]/td/table/tbody/tr/td/table/tbody/tr/td[1]/table/tbody/tr")
+    _LOGIN_ROWS_XPATH = "./table/tbody/tr[1]/td/table/tbody/tr"
+    _DASHBOARD = (By.XPATH, "/html/body/div[2]/div/table")
 
     def __init__(self):
         self.driver_type = None
@@ -27,7 +27,7 @@ class Neobux:
                 options = Options()
 #                options.headless = True
                 self.driver = webdriver.Firefox(options = options, service_log_path = os.path.devnull)
-                self.driver_type = 'geckodriver'
+                self.driver_type = "geckodriver"
             except WebDriverException:
                 self.driver_type = None
                 del options
@@ -38,119 +38,142 @@ class Neobux:
                 options = Options()
                 options.headless = True
                 self.driver = webdriver.Chrome(options = options, service_log_path = os.path.devnull)
-                self.driver_type = 'chromedriver'
+                self.driver_type = "chromedriver"
             except WebDriverException:
                 self.driver_type = None
                 del options
                 del self.driver
         if not self.driver_type:
             self.driver = webdriver.PhantomJS(service_log_path = os.path.devnull)
-            self.driver_type = 'ghostdriver'
+            self.driver_type = "ghostdriver"
         self.actions = ActionChains(self.driver)
         self.load = WebDriverWait(self.driver, 5, poll_frequency = 0.2)
         self.wait = WebDriverWait(self.driver, 60)
+        self.username = None
+        self.password = None
+        self.secondary_password = None
+        self.captcha_image = None
+        self.captcha = None
         self.stale_ad_count = 0
         self.fresh_ad_count = 0
         self.adprize_count = 0
 
     def action_click(self, element):
-        self.driver.execute_script('return arguments[0].scrollIntoView();', element)
+        self.driver.execute_script("return arguments[0].scrollIntoView();", element)
         self.actions.move_to_element(element).perform()
         element.click()
 
     def launch(self):
-        self.driver.get('https://www.neobux.com/')
-        login = self.load.until(expected_conditions.element_to_be_clickable((By.LINK_TEXT, 'Login')))
+        self.driver.get("https://www.neobux.com/")
+        login = self.load.until(expected_conditions.element_to_be_clickable((By.LINK_TEXT, "Login")))
         login.click()
-        self.load.until(expected_conditions.element_to_be_clickable((By.ID, 'loginform')))
+        self.load.until(expected_conditions.element_to_be_clickable((By.ID, "loginform")))
+
+    def prompt_login(self):
+            self.username = input("Username: ")
+            self.password = getpass.getpass()
+            self.secondary_password = getpass.getpass("Secondary Password: ")
+            self.captcha = input("Verification Code: ")
+
+    def get_captcha(self):
+        login_form = self.driver.find_element_by_id("loginform")
+        input_rows = login_form.find_elements_by_xpath(Neobux._LOGIN_ROWS_XPATH)
+        if len(input_rows) == 4:
+            captcha = input_rows[3].find_element_by_xpath("./td/table/tbody/tr/td[@align='right']/img")
+            captcha_input = input_rows[3].find_element_by_xpath("./td/table/tbody/tr/td[@align='left']/input")
+            src = captcha.get_attribute("src")
+            base64_data = src.replace("data:image/png;base64,", "")
+            data = base64.b64decode(base64_data)
+            self.captcha_image = Image.open(BytesIO(data))
+            self.captcha_image = Image.composite(image, Image.new("RGB", image.size, "white"), image)
+            return True
+        else:
+            return False
 
     def log_in(self):
-        while self.driver.find_elements_by_id('loginform'):
-            login_form = self.driver.find_element_by_id('loginform')
-            input_rows = login_form.find_elements_by_xpath(Neobux._LOGIN_ROWS_XPATH)
-            username_input = input_rows[0].find_element_by_xpath('./td/input[@placeholder="Username"]')
-            password_input = input_rows[1].find_element_by_xpath('./td/input[@placeholder="Password"]')
-            secondary_password_input = input_rows[2].find_element_by_xpath('./td/input[@placeholder="Secondary Password"]')
-            username = input('Username: ')
-            password = getpass.getpass()
-            secondary_password = getpass.getpass('Secondary Password: ')
-            if len(input_rows) == 4:
-                captcha = input_rows[3].find_element_by_xpath('./td/table/tbody/tr/td[@align="right"]/img')
-                captcha_input = input_rows[3].find_element_by_xpath('./td/table/tbody/tr/td[@align="left"]/input')
-                src = captcha.get_attribute('src')
-                base64_data = src.replace('data:image/png;base64,', '')
-                data = base64.b64decode(base64_data)
-                image = Image.open(BytesIO(data))
-                image = Image.composite(image, Image.new('RGB', image.size, 'white'), image).show()
-                image.show()
-                solution = input("CAPTCHA: ")
-                captcha_input.send_keys(solution)
-            send = login_form.find_element_by_link_text('send')
-            username_input.click()
-            username_input.send_keys(username)
-            password_input.click()
-            password_input.send_keys(password)
-            secondary_password_input.click()
-            secondary_password_input.send_keys(secondary_password)
-            send.click()
-        self.load.until(expected_conditions.element_to_be_clickable(Neobux._DASHBOARD))
+        login_form = self.driver.find_element_by_id("loginform")
+        input_rows = login_form.find_elements_by_xpath(Neobux._LOGIN_ROWS_XPATH)
+        username_input = input_rows[0].find_element_by_xpath("./td/input[@placeholder='Username']")
+        password_input = input_rows[1].find_element_by_xpath("./td/input[@placeholder='Password']")
+        secondary_password_input = input_rows[2].find_element_by_xpath("./td/input[@placeholder='Secondary Password']")
+        if get_captcha():
+            self.captcha_image.show()
+            captcha_input.send_keys(solution)
+        send = login_form.find_element_by_link_text("send")
+        username_input.click()
+        username_input.send_keys(self.username)
+        password_input.click()
+        password_input.send_keys(self.password)
+        secondary_password_input.click()
+        secondary_password_input.send_keys(self.secondary_password)
+        send.click()
+        try:
+            self.load.until(expected_conditions.element_to_be_clickable(Neobux._DASHBOARD))
+            return True
+        except TimeoutException:
+            return False
 
     def view_ads(self):
-        view = self.driver.find_element_by_link_text('View Advertisements')
+        view = self.driver.find_element_by_link_text("View Advertisements")
         view.click()
-        self.load.until(expected_conditions.element_to_be_clickable((By.CLASS_NAME, 'cell')))
+        self.load.until(expected_conditions.element_to_be_clickable((By.CLASS_NAME, "cell")))
 
     def click_ads(self):
-        ads = self.driver.find_elements_by_class_name('cell')
+        ads = self.driver.find_elements_by_class_name("cell")
         for ad in ads:
+            print("stale ads: %i, clicked ads: %i" % (self.stale_ad_count, self.fresh_ad_count), end= "\r", flush=True)
             self.action_click(ad)
-            self.load.until(lambda d : 'Click the red dot' in ad.text)
-            dot = ad.find_element_by_tag_name('img')
+            self.load.until(lambda d : "Click the red dot" in ad.text)
+            dot = ad.find_element_by_tag_name("img")
             self.action_click(dot)
             self.driver.switch_to.window(self.driver.window_handles[1])
             header = self.load.until(expected_conditions.element_to_be_clickable(Neobux._AD_HEADER))
-            if 'You already saw this advertisement' in header.text:
+            if "You already saw this advertisement" in header.text:
                 self.stale_ad_count += 1
                 self.driver.close()
                 self.driver.switch_to.window(self.driver.window_handles[0])
-                adprize = self.driver.find_element_by_id('adprize').find_element_by_xpath('../div/div[2]')
+                adprize = self.driver.find_element_by_id("adprize").find_element_by_xpath("../div/div[2]")
                 self.adprize_count = int(adprize.text)
                 self.actions = ActionChains(self.driver)
                 continue
-            self.wait.until(expected_conditions.text_to_be_present_in_element(Neobux._AD_HEADER, 'Advertisement validated!'))
-            close = header.find_element_by_link_text('Close')
+            self.wait.until(expected_conditions.text_to_be_present_in_element(Neobux._AD_HEADER, "Advertisement validated!"))
+            close = header.find_element_by_link_text("Close")
             close.click()
             self.fresh_ad_count += 1
             self.driver.switch_to.window(self.driver.window_handles[0])
             self.actions.reset_actions()
             self.actions = ActionChains(self.driver)
+        print("")
 
     def click_adprize(self):
-        adprize = self.driver.find_element_by_id('adprize').find_element_by_xpath('../div/div[2]')
+        adprize = self.driver.find_element_by_id("adprize").find_element_by_xpath("../div/div[2]")
         self.adprize_count = int(adprize.text)
         if self.adprize_count > 0:
             adprize.click()
             self.driver.switch_to.window(self.driver.window_handles[1])
             while True:
+                print("Adprize remaining: %i" % (self.adprize_count), end= "\r", flush=True)
                 try:
                     header = self.load.until(expected_conditions.element_to_be_clickable(Neobux._AD_HEADER))
-                    self.wait.until(expected_conditions.text_to_be_present_in_element(Neobux._AD_HEADER, 'Advertisement validated!'))
-                    self.load.until(lambda d : header.find_element_by_id('rmnDv').text)
-                    self.adprize_count = int(header.find_element_by_id('rmnDv').text)
-                    next = header.find_element_by_link_text('Next')
+                    self.wait.until(expected_conditions.text_to_be_present_in_element(Neobux._AD_HEADER, "Advertisement validated!"))
+                    self.load.until(lambda d : header.find_element_by_id("rmnDv").text)
+                    self.adprize_count = int(header.find_element_by_id("rmnDv").text)
+                    next = header.find_element_by_link_text("Next")
                     next.click()
                 except TimeoutException:
-                    close = header.find_element_by_link_text('Close')
+                    close = header.find_element_by_link_text("Close")
                     close.click()
                     self.driver.switch_to.window(self.driver.window_handles[0])
                     break
 
-    def run(self):
-        self.launch()
-        self.log_in()
-        self.view_ads()
-        self.click_ads()
-        self.click_adprize()
-
     def __del__(self):
         self.driver.quit()
+
+if __name__ == "__main__":
+    clicker = Neobux()
+    clicker.launch()
+    clicker.prompt_login()
+    clicker.log_in()
+    clicker.view_ads()
+    clicker.click_ads()
+    clicker.click_adprize()
--- gui.py
+++ gui.py
@@ -1,7 +1,7 @@
 from Neobux import Neobux
 
 import tkinter
-import idlelib.tooltip
+from idlelib.tooltip import OnHoverTooltipBase
 from base64 import b64decode
 from PIL import Image
 from PIL import ImageTk
@@ -9,20 +9,20 @@ from io import BytesIO
 
 class Application(tkinter.Tk):
 
-    LOADING_IMAGE_BASE64 = ("iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAACuklEQVRYhcWXoW/iUBzH39+AQC2E"
-                            "VCCqSFOBQqAqUBNViKoKVEUVAlWBqqpAIJ5CVBAEqoJkC4yVDdiNIwOyscDWjG0sd7vMf08svNDb"
-                            "kuMubfdLvmlVP5/3e7/3khKyZzUeGzAWBuSxDMEVwPd4xI/jEFwB+37jv4p6FPJYRqKTQOwohthR"
-                            "DPHjOOLHcfYeCthe21AmCrguh4POAYPuJnYUQ26QC16AehTZ8ywSncSHfCYSKNxaWeB7PLgut5eA"
-                            "NJKCE6AeheAK4LocE0h2kz545iwDZaJAn+swlybMpRmMQOOxgdwgh9RJClyXQ+okhWQ3yZI9z8Ja"
-                            "WeFNfHFaBN/jkTpJ+cJ1OehzPfyjlnbT4Hu8T4Lv8TAWRrhwQghRr1Sk3bRPgu/x4a+ckPfzLvZF"
-                            "pN00BFdgz8Nvh+HDCSHEWBgMLPZFCK4AwRXCHbjdUiYKg4p9EWJfDPZs/63yF3lkzjK+aDMtOoHs"
-                            "eRa7yZxlULmtfJ1AbpBD9a76tQKRDSAhhEgjCblBDtJIYgnsft+n5LHsg0sjCeWbcnQC2kzzwfMX"
-                            "eSgTJToBc2ky8PYpj2VQj0YjYa9t5C/yLPJYhjyWo70L9Lnugxe+F6BMlOiGsf5QZ/BdAWWiRLcV"
-                            "xsJA4XuBwdUrFeqVCm2moXZfC1+i+dSEeqV+KlC6LsFcmmg9t8IVqT/UWeuViQJtpkGf6yhdl1C+"
-                            "KcNcmqAeRfOpifZLG8PXIS5/XWL6NsX0bRqMHPUoitMiW/1WwFgYqNxWYK0sVO+qqD/U0Xxqwtk4"
-                            "OP15iuHrMDgJe22jdF36IGAsDFgrC7X7GuuEs3HQ+dEJVoCQ95mwVpZPYPsfUL2rgnoU9tpG67mF"
-                            "9ks7+C78KbLdAnNpwlpZoB79tAvbmQhUghBCnI0De22DepRtAfUoGo8NOBuHDeW/wn8DCAIBcTEu"
-                            "hj0AAAAASUVORK5CYII=")
+    _LOADING_IMAGE_BASE64 = ("iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAACuklEQVRYhcWXoW/iUBzH39+AQC2E"
+                             "VCCqSFOBQqAqUBNViKoKVEUVAlWBqqpAIJ5CVBAEqoJkC4yVDdiNIwOyscDWjG0sd7vMf08svNDb"
+                             "kuMubfdLvmlVP5/3e7/3khKyZzUeGzAWBuSxDMEVwPd4xI/jEFwB+37jv4p6FPJYRqKTQOwohthR"
+                             "DPHjOOLHcfYeCthe21AmCrguh4POAYPuJnYUQ26QC16AehTZ8ywSncSHfCYSKNxaWeB7PLgut5eA"
+                             "NJKCE6AeheAK4LocE0h2kz545iwDZaJAn+swlybMpRmMQOOxgdwgh9RJClyXQ+okhWQ3yZI9z8Ja"
+                             "WeFNfHFaBN/jkTpJ+cJ1OehzPfyjlnbT4Hu8T4Lv8TAWRrhwQghRr1Sk3bRPgu/x4a+ckPfzLvZF"
+                             "pN00BFdgz8Nvh+HDCSHEWBgMLPZFCK4AwRXCHbjdUiYKg4p9EWJfDPZs/63yF3lkzjK+aDMtOoHs"
+                             "eRa7yZxlULmtfJ1AbpBD9a76tQKRDSAhhEgjCblBDtJIYgnsft+n5LHsg0sjCeWbcnQC2kzzwfMX"
+                             "eSgTJToBc2ky8PYpj2VQj0YjYa9t5C/yLPJYhjyWo70L9Lnugxe+F6BMlOiGsf5QZ/BdAWWiRLcV"
+                             "xsJA4XuBwdUrFeqVCm2moXZfC1+i+dSEeqV+KlC6LsFcmmg9t8IVqT/UWeuViQJtpkGf6yhdl1C+"
+                             "KcNcmqAeRfOpifZLG8PXIS5/XWL6NsX0bRqMHPUoitMiW/1WwFgYqNxWYK0sVO+qqD/U0Xxqwtk4"
+                             "OP15iuHrMDgJe22jdF36IGAsDFgrC7X7GuuEs3HQ+dEJVoCQ95mwVpZPYPsfUL2rgnoU9tpG67mF"
+                             "9ks7+C78KbLdAnNpwlpZoB79tAvbmQhUghBCnI0De22DepRtAfUoGo8NOBuHDeW/wn8DCAIBcTEu"
+                             "hj0AAAAASUVORK5CYII=")
                             
 
     def __init__(self, master = None, title = "clicker"):
@@ -44,54 +44,75 @@ class Application(tkinter.Tk):
         self.username_frame = tkinter.Frame(self.login_prompt)
         self.username_frame.grid(row = 0, column = 0, sticky = tkinter.E)
         self.username_label = tkinter.Label(self.username_frame, text = "Username:")
-        self.username_label.grid(row = 0, column = 0, sticky = tkinter.E)
+        self.username_label.grid(row = 0, column = 0)
         self.username_entry = tkinter.Entry(self.username_frame, exportselection = 0)
-        self.username_entry.grid(row = 0, column = 1, sticky = tkinter.W)
+        self.username_entry.grid(row = 0, column = 1)
 
         self.password_frame = tkinter.Frame(self.login_prompt)
         self.password_frame.grid(row = 1, column = 0, sticky = tkinter.E)
         self.password_label = tkinter.Label(self.password_frame, text = "Password:")
-        self.password_label.grid(row = 0, column = 0, sticky = tkinter.E)
+        self.password_label.grid(row = 0, column = 0)
         self.password_entry = tkinter.Entry(self.password_frame, show = u"\u2022")
-        self.password_entry.grid(row = 0, column = 1, sticky = tkinter.W)
+        self.password_entry.grid(row = 0, column = 1)
 
         self.secondary_password_frame = tkinter.Frame(self.login_prompt)
         self.secondary_password_frame.grid(row = 2, column = 0, sticky = tkinter.E)
         self.secondary_password_label = tkinter.Label(self.secondary_password_frame, text = "Secondary Password:")
-        self.secondary_password_label.grid(row = 0, column = 0, sticky = tkinter.E)
+        self.secondary_password_label.grid(row = 0, column = 0)
         self.secondary_password_entry = tkinter.Entry(self.secondary_password_frame, show = u"\u2022")
-        self.secondary_password_entry.grid(row = 0, column = 1, sticky = tkinter.W)
-        self.secondary_password_tooltip = Tooltip(self.secondary_password_frame,
-                                                  "Leave blank if you do not\nhave a secondary password")
+        self.secondary_password_entry.grid(row = 0, column = 1)
+        self.secondary_password_tooltip = Tooltip(self.secondary_password_frame, "Leave blank if you do not\nhave a secondary password")
+
+        self.captcha_frame = tkinter.Frame(self.login_prompt)
+        self.captcha_frame.grid(row = 3, column = 0, sticky = tkinter.E)
+        self.captcha_label = tkinter.Label(self.captcha_frame, text = "Verification Code:")
+        self.captcha_label.grid(row = 0, column = 0, columnspan = 2)
+        self.captcha_entry = tkinter.Entry(self.captcha_frame)
+        self.captcha_entry.grid(row = 0, column = 2)
+        self.captcha_image = ImageTk.PhotoImage(self.driver.captcha_image)
 
         self.login_button = tkinter.Button(self.login_prompt, text = "Login", command = self.neobux_login)
-        self.login_button.grid(row = 3, column = 0)
+        self.login_button.grid(row = 4, column = 0)
+
+        self.login_status = tkinter.Label(self.login_prompt)
+        self.login_status.grid(row = 5, column = 0)
 
     def neobux_login(self):
+        self.login_status.grid_forget()
         self.username_entry.config(state = tkinter.DISABLED)
+        self.driver.username = self.username_entry.get()
         self.password_entry.config(state = tkinter.DISABLED)
+        self.driver.password = self.password_entry.get()
         self.secondary_password_entry.config(state = tkinter.DISABLED)
+        self.driver.secondary_password = self.secondary_password_entry.get()
+        self.loading_graphic.grid(row = 1, column = 0)
+        if self.driver.log_in():
+            self.login_prompt.grid_forget()
+        self.username_entry.config(state = tkinter.NORMAL)
+        self.password_entry.config(state = tkinter.NORMAL)
+        self.secondary_password_entry.config(state = tkinter.NORMAL)
+            
 
     def init_loading_graphic(self):
         self.loading_graphic = tkinter.Canvas(self, height = 32, width = 32, highlightthickness = 0)
-        self.rotate_load_graphic = self.update_loading_graphic().__next__
+        self.rotate_load_graphic = self._update_loading_graphic().__next__
         self.rotate_load_graphic()
     
-    def update_loading_graphic(self):
-        loading_image = Image.open(BytesIO(b64decode(Application.LOADING_IMAGE_BASE64)))
+    def _update_loading_graphic(self):
+        loading_image = Image.open(BytesIO(b64decode(Application._LOADING_IMAGE_BASE64)))
         angle = 0
         while True:
             image = ImageTk.PhotoImage(loading_image.rotate(angle))
             canvas_object = self.loading_graphic.create_image(0, 0, image = image, anchor = tkinter.NW)
-            self.after(34, self.rotate_load_graphic)
+            self.after(58, self.rotate_load_graphic)
             yield
             self.loading_graphic.delete(canvas_object)
-            angle -= 12
+            angle -= 15
             angle %= 360
 
-class Tooltip(idlelib.tooltip.OnHoverTooltipBase):
-    def _init__(self, anchor_widget, text, hover_delay = 500):
-        super(Tooltip, self)._init__(anchor_widget, hover_delay = hover_delay)
+class Tooltip(OnHoverTooltipBase):
+    def __init__(self, anchor_widget, text, hover_delay = 500):
+        super(Tooltip, self).__init__(anchor_widget, hover_delay = hover_delay)
         self.text = text
 
     def showcontents(self):
